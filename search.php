3f5e11dc1629b3e618228bf22af944ae50b68eed
 user <user@mac-user.fritz.box>
2012-01-23 22:08:06 +0100

    search page added

--- search.php
+++ search.php
@@ -2,11 +2,16 @@
 
 <div id="page">
 			<div id="articles">
-				<?php if ( have_posts() ) : ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-						<?php get_template_part( 'content-post', 'post' ); ?>
-					<?php endwhile; ?>
-				<?php endif; ?>
+				<?php
+					if( have_posts()){
+						while( have_posts() ) {
+							the_post();	
+							get_template_part( 'content', 'excerpt' );
+						}
+					} else {
+						echo "nothing found";
+					}
+				?>
 			</div><!-- #articles -->
 		</div><!-- #content -->
 
